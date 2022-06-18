ed2c6e268c4b038e24cf23329a6fa2524965ffbb
Jet <l7egnf@gmail.com>
2022-06-18 17:45:27 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from distutils.core import setup
 
+
 setup(
-    name="JetNetStalker",
+    name="stalker",
     version="1.0",
     description="Allow to download data from https://www.imgur.com",
     author="Jet Free",
     author_email="l7egnf@gmail.com",
     packages=["scripts"],
     package_dir={'scripts': 'scripts'},
-    package_data={'scripts': ['*.dat']},
+    package_data={'scripts': ['*.dat']}
 )
